ced445a533e1523fe55b50f46750d818564c622d
SUSHANTHIP <88308613+SUSHANTHIP@users.noreply.github.com>
2021-10-23 20:40:34 +0530

    Add files via upload

---
+++ java/os/OnlineShopping/App.java
@@ -0,0 +1,13 @@
+package os.OnlineShopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
